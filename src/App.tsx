8336d15bb2a751d14aeb2d748469f0b5cf0bb956
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-07 23:44:29 +0200

    Install Chakra

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div>
+      <ButtonGroup gap="4" m="10">
+        <Button colorScheme="purple">Purple</Button>
+        <Button colorScheme="orange">Orange</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default App;
