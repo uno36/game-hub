b9af006e33f4a2558a3a56210cf092e394209994
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-08 00:02:04 +0200

    Build a responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <ButtonGroup gap="4" m="10">
-        <Button colorScheme="purple">Purple</Button>
-        <Button colorScheme="orange">Orange</Button>
-      </ButtonGroup>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav""aside main"` }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Nav
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Nav
+        </GridItem>
+      </Grid>
     </div>
   );
 }
