448bade5337d7dbdda266214e25549cb0a30681b
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-08 16:41:57 +0200

    Fetch Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" >
-            Nav
+            <GenreList />
           </GridItem>
         </Show>
         <GridItem area="main" >
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,7 @@ const GameCard = ({ game }: Props) => {
     <Card height='300px'>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Heading fontSize={"xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
