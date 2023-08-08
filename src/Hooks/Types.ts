17902d84350f4788ba707a1f6d27de7f9af9cfc0
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-08 12:50:17 +0200

    Displaying platform icons

--- src/Hooks/Types.ts
+++ src/Hooks/Types.ts
@@ -1,7 +1,14 @@
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform}[];
 }
 
 export interface FetchGamesResponse {
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,  
+  FaApple,
+  FaXbox,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { IconType } from "react-icons/lib/esm/iconBase";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo} from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../Hooks/Types";
+import { HStack, Icon } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    Xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
