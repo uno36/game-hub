4736cc297b5f35ead5dd41a41652603edbb34b50
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-08 13:04:32 +0200

    Displaying Critic score

--- src/Hooks/Types.ts
+++ src/Hooks/Types.ts
@@ -9,6 +9,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: {platform: Platform}[];
+  metacritic: number;
 }
 
 export interface FetchGamesResponse {
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../Hooks/Types";
-import { Card, CardBody, Heading, Image} from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -8,11 +9,16 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
+    <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />       
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack> 
       </CardBody>
     </Card>
   );
